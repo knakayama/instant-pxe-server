d1afb86d6774879c2d9c198d4ff941dba9f9a05e
knakayama <knakayama.sh@gmail.com>
2014-11-16 02:38:54 +0900

    change to abspath

--- site-cookbooks/pxe/recipes/pxe-pdhcp.rb
+++ site-cookbooks/pxe/recipes/pxe-pdhcp.rb
@@ -14,9 +14,8 @@ bash "compile pxe-pdhcp" do
 end
 
 bash "boot pxe-pdhcp" do
-  cwd "#{Chef::Config[:file_cache_path]}"
   code <<-EOT
-  ./pxe-pdhcp/pxe-pdhcp -l 0.0.0.0 -b 255.255.255.255 -t "#{node['local']['ip']}" -d pxelinux.0 &
+  "#{Chef::Config[:file_cache_path]}/pxe-pdhcp/pxe-pdhcp" -l 0.0.0.0 -b 255.255.255.255 -t "#{node['local']['ip']}" -d pxelinux.0 &
   EOT
   not_if "ps auxww | grep -qE '[p]xelinux'"
 end
