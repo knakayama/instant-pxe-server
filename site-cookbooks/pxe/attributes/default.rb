98813aab1a68d462e5992a2a2b855ec394069778
knakayama <knakayama.sh@gmail.com>
2014-11-16 02:24:11 +0900

    change iso type

--- site-cookbooks/pxe/attributes/default.rb
+++ site-cookbooks/pxe/attributes/default.rb
@@ -1,4 +1,4 @@
 default['iso']['version'] = "14.04.1"
-default['iso']['name']    = "ubuntu-#{default['iso']['version']}-server-amd64.iso"
+default['iso']['name']    = "ubuntu-#{default['iso']['version']}-desktop-amd64.iso"
 default['iso']['url']     = "http://ftp.jaist.ac.jp/pub/Linux/ubuntu-releases/#{default['iso']['version']}/#{default['iso']['name']}"
 
