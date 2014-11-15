ce7f7396e04f267acd6ab882b9a7b3006c7b6984
knakayama <knakayama.sh@gmail.com>
2014-11-16 03:25:13 +0900

    u

--- site-cookbooks/pxe/recipes/tftpd-hpa.rb
+++ site-cookbooks/pxe/recipes/tftpd-hpa.rb
@@ -3,17 +3,17 @@ package "tftpd-hpa" do
   action :install
 end
 
-service "tftpd-hpa" do
-  action [:enable, :start]
-  supports :status => true, :restart => true, :reload => true
-end
-
 template "/etc/default/tftpd-hpa" do
   source "tftpd-hpa.erb"
   owner  "root"
   group  "root"
   mode   00644
-  notifies :restart, "service[tftpd-hpa]"
+end
+
+service "tftpd-hpa" do
+  action [:enable, :start]
+  supports :status => true, :restart => true, :reload => true
+  subscribes :reload, "template[/etc/default/tftpd-hpa]", :immediately
 end
 
 directory "/home/#{node['local']['user']}/tftp" do
